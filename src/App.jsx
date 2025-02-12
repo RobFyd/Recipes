227eb84df490baa279879e02f048971e256665a4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-02-12 00:18:08 +0000

    refactor App, Article, and Cookbook components to use RecipeContext for improved state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import { recipes } from "./data/recipes";
 import { List } from "./components/List/List";
 import { Cookbook } from "./components/Cookbook/Cookbook";
+import { RecipeContext } from "./context/RecipeContext";
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState(1);
@@ -12,13 +13,15 @@ function App() {
   );
 
   return (
-    <div className={styles.container}>
-      <List
-        recipes={recipes}
-        onSelectRecipe={(id) => setSelectedRecipeId(id)}
-      />
-      <Cookbook recipe={selectedRecipe} heading="Cookbook - exotic dishes" />
-    </div>
+    <RecipeContext.Provider value={selectedRecipe}>
+      <div className={styles.container}>
+        <List
+          recipes={recipes}
+          onSelectRecipe={(id) => setSelectedRecipeId(id)}
+        />
+        <Cookbook heading="Cookbook - exotic dishes" />
+      </div>
+    </RecipeContext.Provider>
   );
 }
 
