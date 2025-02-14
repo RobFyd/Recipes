0e2bfb3aad304c488aae01dfce38f02f1acdc550
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-02-14 00:03:36 +0000

    add Recipe of the week section in App component and adjust promoted styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { recipes } from "./data/recipes";
 import { List } from "./components/List/List";
 import { Cookbook } from "./components/Cookbook/Cookbook";
 import { RecipeContext } from "./context/RecipeContext";
+import { Recipe } from "./components/Recipe/Recipe";
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState(1);
@@ -21,6 +22,12 @@ function App() {
         />
         <Cookbook heading="Cookbook - exotic dishes" />
       </div>
+      <RecipeContext.Provider value={recipes[3]}>
+        <div className={styles.promoted}>
+          <h2>Recipe of the week:</h2>
+          <Recipe />
+        </div>
+      </RecipeContext.Provider>
     </RecipeContext.Provider>
   );
 }
