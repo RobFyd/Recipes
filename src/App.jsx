6d7b51f320937416f2fb7edd5b3e65ccd61450ef
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-02-16 20:47:02 +0000

    add TopBar component and enhance styling for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import styles from "./App.module.css";
 import { recipes } from "./data/recipes";
 import { List } from "./components/List/List";
 import { Cookbook } from "./components/Cookbook/Cookbook";
-import { RecipeContext } from "./context/RecipeContext";
 import { Recipe } from "./components/Recipe/Recipe";
+import { TopBar } from "./components/TopBar/TopBar";
+import { RecipeContext } from "./context/RecipeContext";
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState(1);
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <>
+      <TopBar />
       <div className={styles.container}>
         <RecipeContext.Provider value={selectedRecipe}>
           <List
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -3,21 +3,26 @@ import { Button } from "../Button/Button";
 import styles from "./TopBar.module.css";
 
 export function TopBar() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-    return (
-        <div className={styles.wrapper}>
-            <span className={styles.title}>Reactowe przepisy</span>
-            {isLoggedIn ? (
-                <>
-                    <span>Zalogowany</span>
-                    <Button onClick={() => setIsLoggedIn(false)}>
-                        Wyloguj
-                    </Button>
-                </>
-            ) : (
-                <Button onClick={() => setIsLoggedIn(true)}>Zaloguj</Button>
-            )}
-        </div>
-    );
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>React recipes</span>
+      {isLoggedIn ? (
+        <>
+          <span className={styles.span}>Logged in</span>
+          <Button
+            className={styles.button}
+            onClick={() => setIsLoggedIn(false)}
+          >
+            Log out
+          </Button>
+        </>
+      ) : (
+        <Button className={styles.button} onClick={() => setIsLoggedIn(true)}>
+          Zaloguj
+        </Button>
+      )}
+    </div>
+  );
 }
